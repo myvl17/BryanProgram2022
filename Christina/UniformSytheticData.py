07635b5401309e6d25592a109977c6e67504968f
cdietrich03 <89944575+cdietrich03@users.noreply.github.com>
2022-06-14 10:26:02 -0500

    Uniform Distribution

---
+++ Christina/UniformSytheticData.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 09:24:23 2022
+
+@author: cdiet
+"""
+#https://linuxhint.com/generating-random-numbers-with-uniform-distribution-in-python/
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+np.random.seed(1)
+
+df1 = pd.DataFrame(np.random.uniform(0, 1, (250, 150)))
+df2 = pd.DataFrame(np.random.uniform(3, 4, (250, 150)))
+
+
+df = pd.concat([df1, df2])
+
+perm = np.random.permutation(df)
+
+dfreal = pd.DataFrame(perm)
+
+np.savetxt('test.out', dfreal)
+
+fig, ax = plt.subplots()
+
+plt.scatter(df[0:249], df[250:499])
+
+plt.show()
+
+
