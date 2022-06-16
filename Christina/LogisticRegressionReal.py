743dac1250e993fc21f59102abd51c49c7f3a76d
cdietrich03 <89944575+cdietrich03@users.noreply.github.com>
2022-06-16 08:56:01 -0500

    Updated functions

--- Christina/LogisticRegressionReal.py
+++ Christina/LogisticRegressionReal.py
@@ -9,39 +9,44 @@ import pandas as pd
 import numpy as np
 
 """
-LogReg Inputs
-dataset: saved txt or csv file with augmented data and labels 
-name: what to call the dataset once it is read in
-feature_cols: All columns that are not the labels
-target: the labels or status column, a string, the name of that column
-in the dataframe
-split: the number of the row in the dataframe where it goes from 
-original to augmented data
-save: what to save the output labels as
+LogReg runs original data with labels and take augmented data without
+labels, runs them through a logistic regression, and outputs predicted
+labels. 
 
-
-Output: Will be an 
+LogReg Inputs:
+    dataset = Saved txt or csv file with original data, augmented data,
+    and labels 
+    feature_cols = All columns that are not the labels, listed as an array
+    target = the labels or status column, a string, the name of that column
+    in the dataframe
+    split = The number of rows in the txt/csv that are original and not 
+    augmented data
+    save = The file where you wish to save the labels
+    
+LogReg Outputs:
+    Outputs a text file that contains all of the predicted labels for the 
+    augmented data.
 """
 
-def LogReg(dataset, name, feature_cols, target, split, save):
+def LogReg(dataset, feature_cols, target, split, save):
     # read in the file
     txt = "txt"
     csv = "csv"
     if csv in dataset:
-        name = pd.read_csv(dataset)
+        data = pd.read_csv(dataset)
         
     elif txt in dataset:
-        name = pd.read_table(dataset, delimiter = " ", header = None)
-        name.rename(columns = {150: target}, inplace = True)
+        data = pd.read_table(dataset, delimiter = " ", header = None)
+        data.rename(columns = {150: target}, inplace = True)
         
-    # Find the ratio 
-    ratio = split / len(name)
+    # Find the ratio so it knows what percent is test vs training
+    ratio = (split / len(data))
 
     # Feature variables
-    X = name[feature_cols]
+    X = data[feature_cols]
     
     # Target variable
-    y = name[target]
+    y = data[target]
     
     # Split both x and y into training and testing sets
     
@@ -66,17 +71,16 @@ def LogReg(dataset, name, feature_cols, target, split, save):
     # create the prediction
     y_pred= logreg.predict(X_test)
     
+    # Save the predictions as text files for later use
     np.savetxt(save, y_pred)
-
-    return y_pred
 
 ## EXAMPLE
 
-feature_cols = []
-for i in range(0, 149, 1):
-    feature_cols.append(i)
+# feature_cols = []
+# for i in range(0, 149, 1):
+#     feature_cols.append(i)
     
-print(LogReg(dataset = 'augmented_original_label.txt', name = "data",
-             feature_cols = feature_cols, target = 'status', split = 500,
-             save = 'augmented_data_labels.txt'))
+# print(LogReg(dataset = 'augmented_original_label.txt', name = "data",
+#              feature_cols = feature_cols, target = 'status', split = 500,
+#              save = 'augmented_data_labels.txt'))
 
