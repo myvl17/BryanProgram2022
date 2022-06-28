ea5652737b84a11937b986763cd32ee8ae0def30
Jeffrey-Roberts <jeffrey.roberts@my.simpson.edu>
2022-06-28 18:48:25 -0500

    dataset exploration

---
+++ Jeffrey/Heart Attack Prediction/exploratio.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 27 11:19:38 2022
+
+@author: jeffr
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('datasets/heart.csv')
