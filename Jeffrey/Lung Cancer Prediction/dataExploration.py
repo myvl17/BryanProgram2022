bfc50b41b1109a34e9ddfc3e2d5226943fa15a6b
Jeffrey-Roberts <jeffrey.roberts@my.simpson.edu>
2022-06-29 17:44:38 -0500

    initialize lung cancer dataset

---
+++ Jeffrey/Lung Cancer Prediction/dataExploration.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 13:40:45 2022
+
+@author: jeffr
+"""
+
+import pandas as pd
+import numpy as np
+
+data =  pd.read_csv('lung-cancer.data', sep=",", header=None)
+print(data)
+ 
+df =  pd.read_csv('lung-cancer.data', sep=",", header=None)
