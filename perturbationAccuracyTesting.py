cbc06985f616be038235192d9daeb636513c6090
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-06-29 20:12:17 -0500

    created accuracy graphs

--- perturbationAccuracyTesting.py
+++ perturbationAccuracyTesting.py
@@ -63,118 +63,25 @@ print(accuracy_score(y_test, predicted_values))
 from fixingRandomness import applyAugmentationMethod
 
 
-# pmUnits = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2]
-# accuracy = []
+pmUnits = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 1, 2]
+accuracy = []
 
 
-# for i in range(len(pmUnits)):
-#     augmented = applyAugmentationMethod(df=df, method='pmOne', nrows=4, nvalues=1, unit=pmUnits[i])
+for i in range(len(pmUnits)):
+    plt.clf()
     
-#     from superFunction import logReg
-#     from SVC import SVC
-    
-#     labels = logReg(augmented, [0,1], 2, 10)
-    
-#     titleStr = 'pmOne Accuracy with unit of ' + str(pmUnits[i])
-    
-#     plt.scatter(labels.iloc[:10, 0], labels.iloc[:10, 1], c=labels.iloc[:10, 2], marker='o')
-#     plt.scatter(labels.iloc[10:, 0], labels.iloc[10:, 1], c=labels.iloc[10:, 2], marker='x', s=250)
-#     plt.title(label=titleStr)
-#     plt.show()
-    
-#     test = [2,3,6,8,9,10,11,12,13]
-    
-#     X_train = labels.iloc[training, :2]
-#     y_train = labels.iloc[training, 2]
-    
-#     X_test = labels.iloc[test, :2]
-#     y_test = labels.iloc[test, 2]
-    
-    
-#     from sklearn.svm import SVC
-#     import random
-    
-#     # instantiate the model (using the default parameters)
-#     #random.seed(1)
-#     svm = SVC(gamma = 2, C = 1, kernel = 'linear', max_iter = 10000, random_state = 0)
-    
-#     # fit the model with data
-#     # svm.fit(X_train,y_train)
-#     svm.fit(X_train, y_train)
-#     predicted_values = svm.predict(X_test)
-    
-     
-#     # # Predict on dataset which model has not seen before
-#     # predicted_values = knn.predict(X_test)
-        
-#     (f1_score(y_test, predicted_values))
-#     accuracy.append(accuracy_score(y_test, predicted_values))    
-    
-# plt.plot(pmUnits, accuracy, marker='o')
-# plt.title("pmOne Accuracy Experimentation")
-# plt.xlabel("Unit")
-# plt.ylabel("Accuracy Percent")
-# plt.show()
-
-    
-'''
-augmented = applyAugmentationMethod(df=df, method='gausNoise', nrows=4, nvalues=1, noise=0.5)
-
-from superFunction import logReg
-from SVC import SVC
-
-labels = logReg(augmented, [0,1], 2, 10)
-
-plt.scatter(labels[0], labels[1], c=labels[2])
-plt.show()
-
-test = [2,3,6,8,9,10,11,12,13]
-
-X_train = labels.iloc[training, :2]
-y_train = labels.iloc[training, 2]
-
-X_test = labels.iloc[test, :2]
-y_test = labels.iloc[test, 2]
-
-
-# knn = KNeighborsClassifier(n_neighbors=2)
- 
-# knn.fit(X_train, y_train)
-
-from sklearn.svm import SVC
-import random
-
-# instantiate the model (using the default parameters)
-#random.seed(1)
-svm = SVC(gamma = 2, C = 1, kernel = 'linear', max_iter = 10000, random_state = 0)
-
-# fit the model with data
-# svm.fit(X_train,y_train)
-svm.fit(X_train, y_train)
-predicted_values = svm.predict(X_test)
-
- 
-# # Predict on dataset which model has not seen before
-# predicted_values = knn.predict(X_test)
-    
-(f1_score(y_test, predicted_values))
-print(accuracy_score(y_test, predicted_values))  
-'''
-
-
-
-gausNoise = [0.05, .1, .25, .50, .75, 1, 10, 10.5]
-accuracy=[]
-
-for i in range(len(gausNoise)):
-    augmented = applyAugmentationMethod(df=df, method='gausNoise', nrows=4, nvalues=1, noise=gausNoise[i])
+    augmented = applyAugmentationMethod(df=df, method='pmOne', nrows=4, nvalues=1, unit=pmUnits[i])
     
     from superFunction import logReg
     from SVC import SVC
     
     labels = logReg(augmented, [0,1], 2, 10)
     
-    plt.scatter(labels[0], labels[1], c=labels[2])
+    titleStr = 'pmOne Accuracy with unit of ' + str(pmUnits[i])
+    
+    plt.scatter(labels.iloc[:10, 0], labels.iloc[:9, 1], c=labels.iloc[:10, 2], marker='o')
+    plt.scatter(labels.iloc[10:, 0], labels.iloc[10:, 1], c=labels.iloc[10:, 2], marker='x', s=250)
+    plt.title(label=titleStr)
     plt.show()
     
     test = [2,3,6,8,9,10,11,12,13]
@@ -185,10 +92,6 @@ for i in range(len(gausNoise)):
     X_test = labels.iloc[test, :2]
     y_test = labels.iloc[test, 2]
     
-    
-    # knn = KNeighborsClassifier(n_neighbors=2)
-     
-    # knn.fit(X_train, y_train)
     
     from sklearn.svm import SVC
     import random
@@ -207,9 +110,65 @@ for i in range(len(gausNoise)):
     # predicted_values = knn.predict(X_test)
         
     (f1_score(y_test, predicted_values))
-    accuracy.append(accuracy_score(y_test, predicted_values))  
+    accuracy.append(accuracy_score(y_test, predicted_values))    
+    
+plt.plot(pmUnits, accuracy, marker='o')
+plt.title("pmOne Accuracy Experimentation")
+plt.xlabel("Unit")
+plt.ylabel("Accuracy Percent")
 plt.show()
-plt.plot(gausNoise, accuracy, marker='o')
+
+# gausNoise = [0.05, .1, .25, .50, .75, 1, 10, 10.5]
+# accuracy=[]
+
+# for i in range(len(gausNoise)):
+#     augmented = applyAugmentationMethod(df=df, method='gausNoise', nrows=4, nvalues=1, noise=gausNoise[i])
+    
+#     from superFunction import logReg
+#     from SVC import SVC
+    
+#     titleStr = "gausNoise Accuracy with noise of " + str(gausNoise[i] * 100) + "%"
+    
+#     labels = logReg(augmented, [0,1], 2, 10)
+    
+#     plt.scatter(labels.iloc[:10, 0], labels.iloc[:10, 1], c=labels.iloc[:10, 2], marker='o')
+#     plt.scatter(labels.iloc[10:, 0], labels.iloc[10:, 1], c=labels.iloc[10:, 2], marker='x', s=250)
+#     plt.title(label=titleStr)
+#     plt.show()
+    
+#     test = [2,3,6,8,9,10,11,12,13]
+    
+#     X_train = labels.iloc[training, :2]
+#     y_train = labels.iloc[training, 2]
+    
+#     X_test = labels.iloc[test, :2]
+#     y_test = labels.iloc[test, 2]
+    
+    
+#     # knn = KNeighborsClassifier(n_neighbors=2)
+     
+#     # knn.fit(X_train, y_train)
+    
+#     from sklearn.svm import SVC
+#     import random
+    
+#     # instantiate the model (using the default parameters)
+#     #random.seed(1)
+#     svm = SVC(gamma = 2, C = 1, kernel = 'linear', max_iter = 10000, random_state = 0)
+    
+#     # fit the model with data
+#     # svm.fit(X_train,y_train)
+#     svm.fit(X_train, y_train)
+#     predicted_values = svm.predict(X_test)
+    
+     
+#     # # Predict on dataset which model has not seen before
+#     # predicted_values = knn.predict(X_test)
+        
+#     (f1_score(y_test, predicted_values))
+#     accuracy.append(accuracy_score(y_test, predicted_values))  
+# plt.show()
+# plt.plot(gausNoise, accuracy, marker='o')
 
     
 
@@ -263,4 +222,7 @@ plt.plot(gausNoise, accuracy, marker='o')
 # plt.show()
 # plt.plot(numSwaps, accuracy)  
 # plt.show()
+
+
+
     
