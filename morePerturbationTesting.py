2bc35cb9aeb861a3255d76d8092ef9e0b8c4d609
Jeffrey-Roberts <jeffrey.roberts@my.simpson.edu>
2022-07-05 14:53:56 -0500

    perturbation accuracy testing

--- morePerturbationTesting.py
+++ morePerturbationTesting.py
@@ -15,22 +15,73 @@ from superFunction import logReg
 from superFunction import runClassifier
 
 
-# data = generateRawData(500,150 , -5, 'gaussian')
+# data = generateRawData(500, 150 , -1.1, 'uniform')
 
-plt.scatter(data[0], data[1], c=data[150])
-plt.show()
+print(runClassifier(data, 'SVM', 'f1'))
+
+# plt.scatter(data[0], data[1], c=data[data.shape[1]-1])
+# plt.show()
+# plt.close()
 
 feature_cols = []
 for i in range(data.shape[1]-1):
     feature_cols.append(i)
 
 
-for i in range(1):
-    aug = applyAugmentationMethod(data, 'pmOne', 500, 30, unit=10)
-    log = logReg(dataset=aug, feature_cols=feature_cols, target=150, split=500)
-    acc = runClassifier(log, 'SVM'), 'f1'
+
+pmOneUnit = [0, 0.1, 0.2, 0.5, 0.75]
+pmAccuracy = []
+
+for i in range(len(pmOneUnit)):
+    aug = applyAugmentationMethod(data, 'pmOne', 500, 30, unit=pmOneUnit[i])
+    log = logReg(dataset=aug, feature_cols=feature_cols, target=data.shape[1]-1, split=data.shape[0])
+    acc = runClassifier(log, 'SVM', 'f1')
     
-    print(acc)
     
-    plt.scatter(aug[0], aug[1], c=aug[150])
-    print("HELLO")
+    # print(acc.iloc[0, 3])
+    pmAccuracy.append(acc.iloc[0, 3])
+    
+    # plt.scatter(aug[0], aug[1], c=aug[aug.shape[1]-1])
+    # plt.show()
+    
+# plt.cla()
+plt.plot(pmOneUnit, pmAccuracy)
+plt.show()
+
+
+# gausNoise = [0, 0.05, .25, .5, .75, 1]
+# gausNoiseAcc = []
+
+# for i in range(len(gausNoise)):
+#     aug = applyAugmentationMethod(data, 'gausNoise', 500, 30, noise=gausNoise[i])
+#     log = logReg(dataset=aug, feature_cols=feature_cols, target=data.shape[1]-1, split=data.shape[0])
+#     acc = runClassifier(log, 'SVM', 'f1')
+    
+    
+#     # print(acc.iloc[0, 3])
+#     gausNoiseAcc.append(acc.iloc[0, 3])
+    
+#     # plt.scatter(aug[0], aug[1], c=aug[aug.shape[1]-1])
+#     # plt.show()
+    
+# plt.plot(gausNoise, gausNoiseAcc)
+# plt.show()
+
+
+# randSwap = [0, 1, 5, 25, 50, 75, 100, 150]
+# randSwapAcc = []
+
+# for i in range(len(randSwap)):
+#     aug = applyAugmentationMethod(data, 'randSwap', 500, randSwap[i])
+#     log = logReg(dataset=aug, feature_cols=feature_cols, target=data.shape[1]-1, split=data.shape[0])
+#     acc = runClassifier(log, 'SVM', 'f1')
+    
+    
+#     # print(acc.iloc[0, 3])
+#     randSwapAcc.append(acc.iloc[0, 3])
+    
+#     # plt.scatter(aug[0], aug[1], c=aug[aug.shape[1]-1])
+#     # plt.show()
+    
+# plt.plot(randSwap, randSwapAcc)
+# plt.show()
