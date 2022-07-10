5025cec4388b1d0d99a32e8682d47750f54c5453
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-07-10 17:27:46 -0500

    added comments

--- evenBetterRandSwap.py
+++ evenBetterRandSwap.py
@@ -12,27 +12,39 @@ import matplotlib.pyplot as plt
 
 from generateRawData import generateRawData
 
+
+'''
+DESCRIPTION: randomly selects unique column values and replaces with new value
+from same column
+
+INPUTS:
+data = dataframe
+nrows = number of rows that will be created in augmentation
+nvalues = number of values being swapped, limited to column length
+'''
+
 def betterRandSwap(data, nrows, nvalues):
+    # Creates empty dataframe to store augmented rows
     augmentedDf = pd.DataFrame()
     
+    # Copies nrows from original data and appends to augmentedDf
     for i in range(nrows):
         augmentedDf = pd.concat([augmentedDf, data.iloc[[random.randint(0, data.shape[0]-1)]]], ignore_index=True)
         
-    # augmentedDf = augmentedDf.drop(augmentedDf.shape[1]-1, axis=1)
+    # Drops labels column from augmentedDF
+    augmentedDf = augmentedDf.drop(augmentedDf.shape[1]-1, axis=1)
     
-    # TESTING PURPOSES
-    for i in range(augmentedDf.shape[0]):
-        augmentedDf.iloc[i, augmentedDf.shape[1]-1] = 2
-    
+    # Picks UNIQUE column indexes to swap
     columnIndexSwaps = random.sample(range(0, data.shape[1]-1), nvalues)
 
-    
+    # Swaps augmentedDf column value from same column in data
     for i in range(augmentedDf.shape[0]):
         for col in columnIndexSwaps:
             randValue = data.iloc[random.randint(0, data.shape[0]-1), col]
             
             augmentedDf.iloc[i, col] = randValue
         
+    # Combines both data and augmentedDf into one dataframe
     augmentedDf = pd.concat([data, augmentedDf], axis=0, ignore_index=True)
     
     return augmentedDf
