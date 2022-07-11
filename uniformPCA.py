ebcb83cbb65b565ff390af44bc654b89b7ae8a1e
cdietrich03 <89944575+cdietrich03@users.noreply.github.com>
2022-07-11 10:46:24 -0500

    uniform pca

--- uniformPCA.py
+++ uniformPCA.py
@@ -62,6 +62,7 @@ df.rename(columns = {150:'target'}, inplace = True)
 
 import plotly.express as px
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 X = df.iloc[:,:4]
@@ -76,5 +77,39 @@ fig = px.scatter_3d(
     title=f'Total Explained Variance: {total_var:.2f}%',
     labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3'}
 )
-fig.show(method='external')
+fig.show(method = 'external')
+
+
+# import plotly.express as px
+# from sklearn.decomposition import PCA
+# from sklearn import datasets
+# from sklearn.preprocessing import StandardScaler
+
+# df = px.data.iris()
+# features = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+# X = df[features]
+
+# pca = PCA(n_components=2)
+# components = pca.fit_transform(X)
+
+# loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+
+# fig = px.scatter(components, x=0, y=1, color=df['species'])
+
+# for i, feature in enumerate(features):
+#     fig.add_shape(
+#         type='line',
+#         x0=0, y0=0,
+#         x1=loadings[i, 0],
+#         y1=loadings[i, 1]
+#     )
+#     fig.add_annotation(
+#         x=loadings[i, 0],
+#         y=loadings[i, 1],
+#         ax=0, ay=0,
+#         xanchor="center",
+#         yanchor="bottom",
+#         text=feature,
+#     )
+# fig.show()
 
