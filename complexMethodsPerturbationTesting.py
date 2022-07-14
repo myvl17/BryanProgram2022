4c565eaf6be245e4a8cf0dee9d54b28b6110cb74
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-07-13 21:01:09 -0500

    further testing

--- complexMethodsPerturbationTesting.py
+++ complexMethodsPerturbationTesting.py
@@ -16,6 +16,8 @@ from superFunction import logReg
 from columnNumberTesting import runClassifier
 from modifiedGausNoise import modifiedGausNoise
 
+
+
 NROWS = 100
 NCOLS = 75
 
@@ -23,7 +25,7 @@ data = generateRawData(NROWS, NCOLS, .15, 'gaussian')
 raw = runClassifier(data, 'SVM', 'f1')
 print(raw)
 
-ITERATIONS = 1
+ITERATIONS = 10
 
 for j in range(1):
     nCols = np.arange(0, NCOLS+1, 1)
@@ -48,10 +50,54 @@ for j in range(1):
     augAcc[1:] /= ITERATIONS
     augAcc *= 100
     
-    plt.plot(nCols, augAcc, marker='o')
+    plt.plot(nCols, augAcc, marker='o', color='black')
 
 plt.xlabel('# Columns Swapped')
 plt.ylabel('Accuracy %')
-plt.yticks(np.arange(84, 101, 2))
+plt.grid(True)
+plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5))
 plt.tight_layout()
 plt.show()
+
+
+# NROWS = 100
+# NCOLS = 75
+
+# data = generateRawData(NROWS, NCOLS, .15, 'gaussian')
+# raw = runClassifier(data, 'SVM', 'f1')
+# print(raw)
+
+# ITERATIONS = 100
+
+# for j in range(1):
+#     nRows = np.arange(50, 1500, 50)
+#     augAcc = [0] * (len(nRows)-1)
+#     augAcc.insert(0, raw)
+    
+#     counter = 0
+    
+#     for i in range(ITERATIONS):
+#         for n in range(1, len(nRows)):
+#             counter += 1
+#             print(str(counter / (ITERATIONS * (len(nRows)-1))*100)[:4] + '%')
+            
+#             aug = modifiedGausNoise(data, nRows[n], 3)
+            
+#             log = logReg(aug, feature_cols = np.arange(0, aug.shape[1]-1), target=aug.shape[1]-1, split=data.shape[0]-1)
+            
+#             acc = runClassifier(log, 'SVM', 'f1')
+#             augAcc[n] += acc
+    
+#     augAcc = np.asarray(augAcc)
+#     augAcc[1:] /= ITERATIONS
+#     augAcc *= 100
+    
+#     plt.plot(nRows, augAcc, marker='o', color='black')
+
+# plt.title('# of New Observations vs. Accuracy')
+# plt.xlabel('# Rows Augmented')
+# plt.ylabel('Accuracy %')
+# plt.grid(True)
+# plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5))
+# plt.tight_layout()
+# plt.show()
