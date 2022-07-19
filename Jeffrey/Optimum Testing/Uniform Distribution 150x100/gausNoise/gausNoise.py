de3e16483bbb5d577e1dbc267f89b4449dde432c
Jeffrey-Roberts <jeffreyjayroberts@gmail.com>
2022-07-19 18:26:46 -0500

    conducted gausNoise experiment on Uniform distribution

--- Jeffrey/Optimum Testing/Uniform Distribution 150x100/gausNoise/gausNoise.py
+++ Jeffrey/Optimum Testing/Uniform Distribution 150x100/gausNoise/gausNoise.py
@@ -15,76 +15,76 @@ from betterApplyAugmentationMethod import betterApplyAugmentationMethods
 from updatedSuperFunction import logReg
 
 
-distances = np.arange(0, 2.05, .05)
-distAcc = []
+# distances = np.arange(0, 2.05, .05)
+# distAcc = []
 
-for i in range(len(distances)):
-    data = generateRawData(500, 450, distances[i], 'uniform')
-    distAcc.append(runClassifier(data, 'SVM', 'f1'))
+# for i in range(len(distances)):
+#     data = generateRawData(500, 450, distances[i], 'uniform')
+#     distAcc.append(runClassifier(data, 'SVM', 'f1'))
     
     
-fig = plt.subplots(figsize=(20,5))
-plt.plot(distances, distAcc, marker='o')
-plt.xticks(distances)
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+# fig = plt.subplots(figsize=(20,5))
+# plt.plot(distances, distAcc, marker='o')
+# plt.xticks(distances)
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
 
 
 
-NROWS = 150
-NCOLS = 100
+# NROWS = 150
+# NCOLS = 100
 
-data = generateRawData(NROWS, NCOLS, .05, 'uniform')
-raw = runClassifier(data, 'SVM', 'f1')
+# data = generateRawData(NROWS, NCOLS, .05, 'uniform')
+# raw = runClassifier(data, 'SVM', 'f1')
 
-ITERATIONS = 25
+# ITERATIONS = 25
 
-for j in range(1):
-    noise = np.arange(0, 5.1, .25)
-    augAcc = [0] * (len(noise)-1)
-    augAcc.insert(0, raw)
+# for j in range(1):
+#     noise = np.arange(0, 5.1, .25)
+#     augAcc = [0] * (len(noise)-1)
+#     augAcc.insert(0, raw)
     
-    counter = 0
+#     counter = 0
     
-    for i in range(ITERATIONS):
-        for n in range(1, len(noise)):
-            counter += 1
-            print(str(counter / (ITERATIONS * (len(noise)-1))*100)[:4] + '%')
+#     for i in range(ITERATIONS):
+#         for n in range(1, len(noise)):
+#             counter += 1
+#             print(str(counter / (ITERATIONS * (len(noise)-1))*100)[:4] + '%')
             
-            aug = betterApplyAugmentationMethods(data, 'gausNoise', 100, noise=noise[n])
+#             aug = betterApplyAugmentationMethods(data, 'gausNoise', 100, noise=noise[n])
             
-            log = logReg(aug, split=data.shape[0]-1)
+#             log = logReg(aug, split=data.shape[0]-1)
             
-            acc = runClassifier(log, 'SVM', 'f1')
-            augAcc[n] += acc
+#             acc = runClassifier(log, 'SVM', 'f1')
+#             augAcc[n] += acc
     
-    augAcc = np.asarray(augAcc)
-    augAcc[1:] /= ITERATIONS
-    augAcc *= 100
+#     augAcc = np.asarray(augAcc)
+#     augAcc[1:] /= ITERATIONS
+#     augAcc *= 100
     
-    plt.plot(noise, augAcc, marker='o', color='black')
+#     plt.plot(noise, augAcc, marker='o', color='black')
 
-plt.xlabel('Noise %')
-plt.ylabel('Accuracy %')
-plt.grid(True)
-plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5))
-plt.tight_layout()
+# plt.xlabel('Noise %')
+# plt.ylabel('Accuracy %')
+# plt.grid(True)
+# plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5))
+# plt.tight_layout()
 
-plt.show()
+# plt.show()
 
 
-fig = plt.subplots(figsize=(20,10))
-plt.plot(noise, augAcc, marker='o', color='black')
-plt.title('Gaus Noise vs. Accuracy', fontsize='xx-large')
-plt.xlabel('Noise %', fontsize='xx-large')
-plt.ylabel('Accuracy %', fontsize='xx-large')
-plt.grid(True)
-plt.xticks(noise)
-plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5), fontsize='xx-large')
-plt.tight_layout()
+# fig = plt.subplots(figsize=(20,10))
+# plt.plot(noise, augAcc, marker='o', color='black')
+# plt.title('Gaus Noise vs. Accuracy', fontsize='xx-large')
+# plt.xlabel('Noise %', fontsize='xx-large')
+# plt.ylabel('Accuracy %', fontsize='xx-large')
+# plt.grid(True)
+# plt.xticks(noise)
+# plt.yticks(np.arange(int(str(raw*100)[0] + '0'), 105, 5), fontsize='xx-large')
+# plt.tight_layout()
 
-plt.show()
+# plt.show()
 
 
 NROWS = 150
@@ -107,7 +107,7 @@ for j in range(1):
             counter += 1
             print(str(counter / (ITERATIONS * (len(nRows)-1))*100)[:4] + '%')
             
-            aug = betterApplyAugmentationMethods(data, 'gausNoise', nRows[n], noise = .25)
+            aug = betterApplyAugmentationMethods(data, 'gausNoise', nRows[n], noise = .5)
             
             log = logReg(aug, split=data.shape[0]-1)
             
